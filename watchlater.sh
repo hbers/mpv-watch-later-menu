e1487c6e90eeedcaa28956622aa99d4a0ab50b2c
hbers <hbersp@gmail.com>
2015-09-05 23:12:26 +0300

    Delete watchlater.sh

--- watchlater.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat $HOME/.config/mpv/watch_later/* | sed 's:.*/::' | grep 'avi\|mp4\|mkv' | sed '/./='  | sed '/./N; s/\n/) /' 
-
-read -p $'What do you want to watch now? \x0a' m
-sleep 1 && kill $$ &
-setsid mpv --really-quiet "$(cat $HOME/.config/mpv/watch_later/* | grep 'avi\|mp4\|mkv' | sed "$m q;d" | sed 's/^..//')" ; exit 
