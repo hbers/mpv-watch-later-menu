aa13815b68f9fb86aca98fc10df69cff0a8a9fba
hbers <hbersp@gmail.com>
2015-09-06 21:28:03 +0300

    added a line

--- watchlater.sh
+++ watchlater.sh
@@ -2,6 +2,6 @@
 
 cat $HOME/.config/mpv/watch_later/* | sed 's:.*/::' | grep 'avi\|mp4\|mkv' | sed '/./='  | sed '/./N; s/\n/) /' 
 
-read -p $'What do you want to watch now? \x0a' m
+read -p $'\x0aWhat do you want to watch now? \x0a' m
 sleep 1 && kill $$ &
 setsid mpv --really-quiet "$(cat $HOME/.config/mpv/watch_later/* | grep 'avi\|mp4\|mkv' | sed "$m q;d" | sed 's/^..//')" ; exit 
